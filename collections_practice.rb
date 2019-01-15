c22121522c7000a112586486a5939d2490762340
JESSICA A BETTS <jessicaannbettsis@gmail.com>
2019-01-15 09:05:02 +0000

    Automatically backed up by Learn

--- collections_practice.rb
+++ collections_practice.rb
@@ -91,7 +91,7 @@ def organize_schools(array)
   
   location_array.each do |item|
     city = item.fetch(:location)
-    new_hash = Hash.new_hash
+    new_hash = Hash.new
     new_hash[city] = []
     location_array2.push(new_hash)
   end
