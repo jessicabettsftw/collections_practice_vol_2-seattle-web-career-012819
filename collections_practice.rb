ff5df5bb93f006f90e3b6ca2dfd9f5d8cbf2c98e
JESSICA A BETTS <jessicaannbettsis@gmail.com>
2019-01-15 09:26:58 +0000

    Automatically backed up by Learn

--- collections_practice.rb
+++ collections_practice.rb
@@ -100,6 +100,7 @@ def organize_schools(array)
     city_array = []
     array.each do |school|
       puts city
+      puts school[1].values
       if city == school[1].values
         puts "#{city} == #{school[1].values}"
         city_array.push(school[0])
